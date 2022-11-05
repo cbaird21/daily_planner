c17f5babc4bb5f3c7aa7fa04dce40c00521e3619
unknown <chandler.baird21@gmail.com>
2022-11-05 13:57:28 -0600

    added times array to js, continuing to work on scroll time function. Date and time both display at the top of the page.

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
-// in the html
-  var userInput = $('textarea.description',this).val();
-  var currentTime = dayjs();
-// $('#currentDay').text(currentTime.$d);
-$(function(){
-  $('#currentDay').text(currentTime.$d);
+// in the html.
+var currentTime = dayjs();
+var currentDay = $("#currentDay");
+var textArea = $("textarea", this).val();
+var appointments = [];
+var hoursEl = $("#hours", this).children();
 
+var timesArr= [];
+i=0;
+$("div.row").each(function (){
+  timesArr[i++] = $(this).attr("data-hour");
+});
+console.log(timesArr);
+
+$(function () {
+  console.log(hoursEl);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  $(document).on('click','.saveBtn', function(){
-    var parentEl=$(this).parent();
-    console.log(parentEl);
+  // local storage. HINT: What does this reference in the click listener
+  $(document).on("click", ".saveBtn", function () {
+    // DOM traversal can be used to get the "hour-x" id of the parent element a.k.a ->
+    var parentEl = $(this).parent();
+
+    var userInputObject = {
+      id: parentEl[0].id,
+      userInput: parentEl[0].children[1].value,
+    };
+    appointments.push(userInputObject);
+    localStorage.setItem("appointments", JSON.stringify(appointments));
+
+    console.log(localStorage.appointments);
     console.log(parentEl[0].id);
-    console.log(userInput);
-    localStorage.setItem();
-  })
-});
+
+    // textArea.text(localStorage.getItem("appointments")) || [];
+
+    console.log(textArea);
+    // for (var i = 0; i < appointments.length; i++){
+    //   textArea.textContent = ${textArea[i].userInput};
+    //   textArea.append('userInput');
+    // }
+  });
+
+  // .then(function displayApp() {
+
+  // })
+  // How might the id be
+  // useful when saving the description in local storage?
+
+  //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -32,3 +60,12 @@ $(function(){
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var day = dayjs();
+  window.onload = displayClock();
+  function displayClock() {
+    var liveTime = new Date().toLocaleTimeString();
+
+    currentDay.text(day.format("MM DD, YYYY") + " " + liveTime);
+    setTimeout(displayClock, 1000);
+  }
+});
